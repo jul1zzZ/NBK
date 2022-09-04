8cf92450332dc3c1a64fbae4fae4098f19c1b434
Jul1zzZ <ruibastia@gmail.com>
2022-09-04 12:51:47 +0300

    12

---
+++ 123/nba/MainScreenPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace nba
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void VisitorBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new VisitorPage());
+        }
+
+        private void AdminBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                BackBtn.Visibility = Visibility.Visible;
+                VisitorBtn.Visibility = Visibility.Collapsed;
+                AdminBtn.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                BackBtn.Visibility = Visibility.Collapsed;
+                VisitorBtn.Visibility = Visibility.Visible;
+                AdminBtn.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                MainFrame.GoBack();
+            }
+        }
+    }
+}
